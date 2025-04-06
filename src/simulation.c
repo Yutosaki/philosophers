4e04c13df7f7da8e2dcf5ef974bab5134b940c88
Yutosaki <sasayu8190@gmail.com>
2025-04-06 12:07:54 +0900

    chore: divide funcs to two files

--- src/simulation.c
+++ src/simulation.c
@@ -6,105 +6,11 @@
 /*   By: yutsasak <yutsasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 15:24:00 by yutsasak          #+#    #+#             */
-/*   Updated: 2025/03/31 21:21:40 by yutsasak         ###   ########.fr       */
+/*   Updated: 2025/04/03 12:20:11 by yutsasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-void	take_forks(t_philo *philo)
-{
-	if (philo->id % 2 == 0)
-	{
-		pthread_mutex_lock(philo->right_fork);
-		print_status(philo, FORK_TAKEN);
-		pthread_mutex_lock(philo->left_fork);
-		print_status(philo, FORK_TAKEN);
-	}
-	else
-	{
-		pthread_mutex_lock(philo->left_fork);
-		print_status(philo, FORK_TAKEN);
-		pthread_mutex_lock(philo->right_fork);
-		print_status(philo, FORK_TAKEN);
-	}
-}
-
-void	eat(t_philo *philo)
-{
-	pthread_mutex_lock(&philo->data->meal_mutex);
-	philo->last_meal_time = get_time();
-	pthread_mutex_unlock(&philo->data->meal_mutex);
-	print_status(philo, EATING);
-	precise_sleep(philo->data->time_to_eat);
-	pthread_mutex_lock(&philo->data->meal_mutex);
-	philo->meals_count++;
-	pthread_mutex_unlock(&philo->data->meal_mutex);
-	pthread_mutex_unlock(philo->left_fork);
-	pthread_mutex_unlock(philo->right_fork);
-}
-
-void	sleep_and_think(t_philo *philo)
-{
-	print_status(philo, SLEEPING);
-	precise_sleep(philo->data->time_to_sleep);
-	print_status(philo, THINKING);
-}
-
-bool	is_dead(t_philo *philo)
-{
-	long long	current_time;
-	long long	time_since_last_meal;
-	bool		is_dead;
-
-	is_dead = false;
-	pthread_mutex_lock(&philo->data->meal_mutex);
-	current_time = get_time();
-	time_since_last_meal = current_time - philo->last_meal_time;
-	if (time_since_last_meal > philo->data->time_to_die)
-		is_dead = true;
-	pthread_mutex_unlock(&philo->data->meal_mutex);
-	if (is_dead)
-	{
-		pthread_mutex_lock(&philo->data->death_mutex);
-		if (!philo->data->someone_died)
-		{
-			philo->data->someone_died = true;
-			pthread_mutex_unlock(&philo->data->death_mutex);
-			print_status(philo, DIED);
-			return (true);
-		}
-		pthread_mutex_unlock(&philo->data->death_mutex);
-	}
-	return (false);
-}
-
-bool	is_ate_enough(t_simulation_data *data, t_philo *philos)
-{
-	int	i;
-	int	count;
-
-	if (data->num_times_to_eat == -1)
-		return (false);
-	count = 0;
-	i = 0;
-	while (i < data->num_of_philos)
-	{
-		pthread_mutex_lock(&data->meal_mutex);
-		if (philos[i].meals_count >= data->num_times_to_eat)
-			count++;
-		pthread_mutex_unlock(&data->meal_mutex);
-		i++;
-	}
-	if (count == data->num_of_philos)
-	{
-		pthread_mutex_lock(&data->death_mutex);
-		data->is_ate_enough = true;
-		pthread_mutex_unlock(&data->death_mutex);
-		return (true);
-	}
-	return (false);
-}
 
 void	*monitor_routine(void *arg)
 {
@@ -130,6 +36,16 @@ void	*monitor_routine(void *arg)
 	return (NULL);
 }
 
+void *case_one_philo(t_philo *philo)
+{
+	pthread_mutex_lock(philo->left_fork);
+	print_status(philo, FORK_TAKEN);
+	precise_sleep(philo->data->time_to_die);
+	pthread_mutex_unlock(philo->left_fork);
+	print_status(philo, DIED);
+	return (NULL);
+}
+
 void	*philosopher_routine(void *arg)
 {
 	t_philo	*philo;
@@ -139,16 +55,9 @@ void	*philosopher_routine(void *arg)
 	philo->last_meal_time = get_time();
 	pthread_mutex_unlock(&philo->data->meal_mutex);
 	if (philo->data->num_of_philos == 1)
-	{
-		pthread_mutex_lock(philo->left_fork);
-		print_status(philo, FORK_TAKEN);
-		precise_sleep(philo->data->time_to_die);
-		pthread_mutex_unlock(philo->left_fork);
-		print_status(philo, DIED);
-		return (NULL);
-	}
+	    return(case_one_philo(philo));
 	if (philo->id % 2 == 0)
-		precise_sleep(philo->data->time_to_eat / 2);
+		precise_sleep(philo->data->time_to_eat / 2); // 待ち時間がちょうど良くなる?
 	while (1)
 	{
 		pthread_mutex_lock(&philo->data->death_mutex);
---
+++ src/simulation_helper.c
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   simulation_helper.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yutsasak <yutsasak@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/03 12:19:50 by yutsasak          #+#    #+#             */
+/*   Updated: 2025/04/04 15:55:01 by yutsasak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void    take_forks(t_philo *philo)
+{
+	if (philo->id % 2 == 0)
+	{
+		pthread_mutex_lock(philo->right_fork);
+		print_status(philo, FORK_TAKEN);
+		pthread_mutex_lock(philo->left_fork);
+		print_status(philo, FORK_TAKEN);
+	}
+	else
+	{
+		pthread_mutex_lock(philo->left_fork);
+		print_status(philo, FORK_TAKEN);
+		pthread_mutex_lock(philo->right_fork);
+		print_status(philo, FORK_TAKEN);
+	}
+}
+
+void	eat(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->data->meal_mutex);
+	philo->last_meal_time = get_time();
+	pthread_mutex_unlock(&philo->data->meal_mutex);
+	print_status(philo, EATING);
+	precise_sleep(philo->data->time_to_eat);
+	pthread_mutex_lock(&philo->data->meal_mutex);
+	philo->meals_count++;
+	pthread_mutex_unlock(&philo->data->meal_mutex);
+	pthread_mutex_unlock(philo->left_fork);
+	pthread_mutex_unlock(philo->right_fork);
+}
+
+void    sleep_and_think(t_philo *philo)
+{
+	print_status(philo, SLEEPING);
+	precise_sleep(philo->data->time_to_sleep);
+	print_status(philo, THINKING);
+}
+
+bool    is_dead(t_philo *philo)
+{
+	long long	current_time;
+	long long	time_since_last_meal;
+	bool		is_dead;
+
+	is_dead = false;
+	pthread_mutex_lock(&philo->data->meal_mutex);
+	current_time = get_time();
+	time_since_last_meal = current_time - philo->last_meal_time;
+	if (time_since_last_meal > philo->data->time_to_die)
+		is_dead = true;
+	pthread_mutex_unlock(&philo->data->meal_mutex);
+	if (is_dead)
+	{
+		pthread_mutex_lock(&philo->data->death_mutex);
+		if (!philo->data->someone_died)
+		{
+			philo->data->someone_died = true;
+			pthread_mutex_unlock(&philo->data->death_mutex);
+			print_status(philo, DIED);
+			return (true);
+		}
+		pthread_mutex_unlock(&philo->data->death_mutex);
+	}
+	return (false);
+}
+
+bool	is_ate_enough(t_simulation_data *data, t_philo *philos)
+{
+	int	i;
+	int	count;
+
+	if (data->must_eat_count == -1)
+		return (false);
+	count = 0;
+	i = 0;
+	while (i < data->num_of_philos)
+	{
+		pthread_mutex_lock(&data->meal_mutex);
+		if (philos[i].meals_count >= data->must_eat_count)
+			count++;
+		pthread_mutex_unlock(&data->meal_mutex);
+		i++;
+	}
+	if (count == data->num_of_philos)
+	{
+		pthread_mutex_lock(&data->death_mutex);
+		data->is_ate_enough = true;
+		pthread_mutex_unlock(&data->death_mutex);
+		return (true);
+	}
+	return (false);
+}
